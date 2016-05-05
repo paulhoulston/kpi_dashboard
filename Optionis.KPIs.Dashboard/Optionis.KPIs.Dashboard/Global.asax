1bec0ce8b1d0adc0f4640cdd8815e502832bc57b
paul.houlston <paul.houlston@hotmail.co.uk>
2016-05-05 20:44:55 +0100

    Added test controller for rest api

---
+++ Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="Optionis.KPIs.Dashboard.Global" %>
---
+++ Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using System.Net.Http.Formatting;
+using System.Linq;
+
+namespace Optionis.KPIs.Dashboard
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Formatters.OfType<JsonMediaTypeFormatter>().First().SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
