dfa47d89af66cabfa734877ae64a78ca7b293df7
paul.houlston <paul.houlston@hotmail.co.uk>
2016-05-05 21:06:40 +0100

    Added releases repository and controller

---
+++ Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard/Controllers/ReleasesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Optionis.KPIs.Dashboard
+{
+    public class ReleasesController : ApiController
+    {
+        readonly IStoreReleases _repository;
+
+        public ReleasesController ()
+        {
+            _repository = new ReleasesRepository ();
+        }
+
+        public dynamic Get()
+        {
+            return new
+            {
+                Releases = _repository.GetAll ()
+            };
+        }
+    }
+
+    interface IStoreReleases
+    {
+        IEnumerable<Release> GetAll();
+    }
+
+    class Release
+    {
+    }
+
+    class ReleasesRepository : IStoreReleases
+    {
+        public IEnumerable<Release> GetAll ()
+        {
+            return new Release[]{ };
+        }
+    }
+}
+
