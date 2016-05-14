fdac1b68d51a428b94cc0c3c2bb02b515c2b3cd7
paul.houlston <paul.houlston@hotmail.co.uk>
2016-05-14 06:25:12 +0100

    Added failing test and back-dated nunit to a version compatibile with Xamarin Studio

--- Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard.Application.Tests/EmptyClass.cs
+++ Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard.Application.Tests/EmptyClass.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using NUnit.Framework;
 
 namespace Optionis.KPIs.Dashboard.Application.Tests
 {
-    public class EmptyClass
+    [TestFixture]
+    public class GIVEN_I_want_to_create_a_release
     {
-        public EmptyClass ()
+        public class WHEN_I_do_supply_a_valid_model
         {
+            [Test]
+            public void THEN_the_release_is_created()
+            {
+                bool releaseCreated = false;
+                Assert.True (releaseCreated);
+            }
         }
     }
 }
