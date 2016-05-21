13abd2f6c96a88f74798fcc7f21bee8b473e87ab
paul.houlston <paul.houlston@hotmail.co.uk>
2016-05-21 20:40:50 +0100

    Added default route

--- Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard/Global.asax.cs
+++ Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard/Global.asax.cs
@@ -1,5 +1,12 @@
 ﻿using System.Web;
 using Nancy;
+using Nancy.Conventions;
+using System.Reflection;
+using System;
+using System.IO;
+using Nancy.Bootstrapper;
+using Nancy.ErrorHandling;
+using Nancy.Responses;
 
 namespace Optionis.KPIs.Dashboard
 {
@@ -10,6 +17,26 @@ namespace Optionis.KPIs.Dashboard
 #if DEBUG
             StaticConfiguration.DisableErrorTraces = false;
 #endif
+        }
+    }
+
+    public class Test : NancyModule
+    {
+        public Test ()
+        {
+            Get ["/test"] = _ => View ["index.html"];   
+        }
+    }
+
+    public class CustomBoostrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions conventions)
+        {
+            base.ConfigureConventions (conventions);
+
+            conventions.StaticContentsConventions.AddDirectory ("Scripts", @"Scripts");
+            conventions.StaticContentsConventions.AddDirectory ("Content", @"Content");
+            conventions.StaticContentsConventions.AddDirectory ("/", @"/Content");
         }
     }
 }
