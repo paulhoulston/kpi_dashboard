58dbb5e6f4cd15e8372bbd7f46c1eff78c8dbe21
paul.houlston <paul.houlston@hotmail.co.uk>
2016-05-22 06:15:02 +0100

    Releases output to html now

---
+++ Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard/Scripts/releases.js
@@ -0,0 +1,14 @@
+﻿var Releases = {
+    init: function() {
+
+        function bindView(opts) {
+            var theTemplateScript = $(opts.template).html(),
+                compiledTemplate = Handlebars.compile (theTemplateScript);
+            $(opts.target).append (compiledTemplate (opts.data));
+        }
+
+        $.getJSON('/releases', function(d){
+            bindView({ template: '#releases-template', target: 'body', data: d });
+        });
+    }
+};
