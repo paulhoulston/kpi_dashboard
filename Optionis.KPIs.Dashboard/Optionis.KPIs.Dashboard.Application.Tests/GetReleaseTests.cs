4d1f22a4e2a91a708f40499e09cdf4426199e990
paul.houlston <paul.houlston@hotmail.co.uk>
2016-05-22 11:19:23 +0100

    Added failing test for when release is not found

---
+++ Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard.Application.Tests/GetReleaseTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Optionis.KPIs.Dashboard.Application.Tests
+{
+    public class GIVEN_I_want_to_get_a_release
+    {
+        public class WHEN_the_release_does_not_exist
+        {
+            [Test]
+            public void THEN_the_release_is_not_returned()
+            {
+                var isNotFound = false;
+
+                Assert.IsTrue (isNotFound);
+            }
+        }
+    }
+}
+
