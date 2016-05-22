6746d550fd6ecac1a64f7626b1bf3e9c87316497
paul.houlston <paul.houlston@hotmail.co.uk>
2016-05-22 09:36:16 +0100

    Individual releases now rendered

--- Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard/Scripts/releases.js
+++ Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard/Scripts/releases.js
@@ -1,14 +1,39 @@
 ﻿var Releases = {
+    
+    settings: {
+        uri: '/releases'
+    },
+
+    templates: {
+        releases: '#releases-template',
+        releaseDetails: '#release-details-template'
+    },
+
+    bindView: function(opts) {
+        var theTemplateScript = $(opts.template).html(),
+            compiledTemplate = Handlebars.compile (theTemplateScript);
+        return $(opts.target).append (compiledTemplate (opts.data));
+    },
+
+    getRelease: function(uri) {
+        $.getJSON(uri, function(d) {
+            Releases.bindView({ 
+                template: Releases.templates.releaseDetails, 
+                target: 'div[data-uri="' + uri + '"]',
+                 data: d
+            });
+        });
+    },
+
     init: function() {
-
-        function bindView(opts) {
-            var theTemplateScript = $(opts.template).html(),
-                compiledTemplate = Handlebars.compile (theTemplateScript);
-            $(opts.target).append (compiledTemplate (opts.data));
-        }
-
-        $.getJSON('/releases', function(d){
-            bindView({ template: '#releases-template', target: 'body', data: d });
+        $.getJSON(Releases.settings.uri, function(d){
+            Releases.bindView({ 
+                template: Releases.templates.releases,
+                target: '#releases',
+                data: d 
+            }).children('div[data-uri]').each(function(i, o) {
+                Releases.getRelease($(o).attr('data-uri'));
+            });
         });
     }
 };
