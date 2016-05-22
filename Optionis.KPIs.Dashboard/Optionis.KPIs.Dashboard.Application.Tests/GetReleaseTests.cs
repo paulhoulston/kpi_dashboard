4722258ff682426f0f609db1ca884df08fe48869
paul.houlston <paul.houlston@hotmail.co.uk>
2016-05-22 11:32:06 +0100

    All tests pass

--- Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard.Application.Tests/GetReleaseTests.cs
+++ Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard.Application.Tests/GetReleaseTests.cs
@@ -5,27 +5,37 @@ namespace Optionis.KPIs.Dashboard.Application.Tests
 {
     public class GIVEN_I_want_to_get_a_release
     {
-        public class WHEN_the_release_does_not_exist
+        public class WHEN_the_release_does_not_exist : GetReleaseService.IGetReleases
         {
+            public void Get (int releaseId, Action onReleaseNotFound, Action onReleaseFound)
+            {
+                onReleaseNotFound ();
+            }
+
             [Test]
             public void THEN_the_release_is_not_returned()
             {
                 var isNotFound = false;
                 var isFound = false;
-                new GetReleaseService (() => isNotFound = true, () => isFound = true).Get();
+                new GetReleaseService (this, () => isNotFound = true, () => isFound = true).Get(0);
                 Assert.IsTrue (isNotFound);
                 Assert.IsFalse (isFound);
             }
         }
 
-        public class WHEN_the_release_exists
+        public class WHEN_the_release_exists : GetReleaseService.IGetReleases
         {
+            public void Get (int releaseId, Action onReleaseNotFound, Action onReleaseFound)
+            {
+                onReleaseFound ();
+            }
+
             [Test]
             public void THEN_the_release_is_returned()
             {
                 var isFound = false;
                 var isNotFound = false;
-                new GetReleaseService (() => isNotFound = true, () => isFound = true).Get();
+                new GetReleaseService (this, () => isNotFound = true, () => isFound = true).Get(0);
                 Assert.IsFalse (isNotFound);
                 Assert.IsTrue (isFound);
             }
@@ -36,16 +46,23 @@ namespace Optionis.KPIs.Dashboard.Application.Tests
     {
         readonly Action _onReleaseNotFound;
         readonly Action _onReleaseFound;
+        readonly IGetReleases _repository;
 
-        public GetReleaseService (Action onReleaseNotFound, Action onReleaseFound)
+        public interface IGetReleases
         {
+            void Get(int releaseId, Action onReleaseNotFound, Action onReleaseFound);
+        }
+
+        public GetReleaseService (IGetReleases repository, Action onReleaseNotFound, Action onReleaseFound)
+        {
+            _repository = repository;
             _onReleaseNotFound = onReleaseNotFound;
             _onReleaseFound = onReleaseFound;
         }
 
-        public void Get ()
+        public void Get (int releaseId)
         {
-            _onReleaseNotFound ();   
+            _repository.Get (releaseId, _onReleaseNotFound, _onReleaseFound);
         }
     }
 }
