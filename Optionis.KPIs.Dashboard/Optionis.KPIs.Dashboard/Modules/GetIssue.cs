0b0c6c56c8281b2cd98796f8921944d5e503355a
paul.houlston <paul.houlston@hotmail.co.uk>
2016-05-27 05:48:22 +0100

    Implemented get issue controller

---
+++ Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard/Modules/GetIssue.cs
@@ -0,0 +1,38 @@
+﻿using Nancy;
+using Optionis.KPIs.Common;
+using Optionis.KPIs.Dashboard.Application;
+using Optionis.KPIs.Dashboard.Adapters;
+
+namespace Optionis.KPIs.Dashboard.Modules
+{
+    public class GetIssue : NancyModule
+    {
+        public GetIssue ()
+        {
+            Get [Routing.Issues.GET] = _ => {
+                Response response = null;
+                new GetIssueService (
+                    new IssueRetriever (),
+                    () => response = HttpStatusCode.NotFound,
+                    issue => {
+                        response = Newtonsoft.Json.JsonConvert.SerializeObject(Convert (issue));
+                        response.StatusCode = HttpStatusCode.OK;
+                        response.ContentType = "application/json";
+                    }
+                ).Get (_.Id);
+                return response;
+            };
+        }
+
+        static dynamic Convert(GetIssueService.Issue issue)
+        {
+            return new {
+                Self = Routing.Issues.Get (issue.Id),
+                issue.IssueId,
+                issue.Link,
+                issue.Title
+            };
+        }
+    }
+}
+
