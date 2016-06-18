d244b4c79d9baa0a5cae620abe94183c21d9688d
paul.houlston <paul.houlston@hotmail.co.uk>
2016-06-18 06:28:23 +0100

    Added failing test that checks if release is deleted if no more deployments are associated

--- Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard.Application.Tests/DeleteDeploymentTests.cs
+++ Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard.Application.Tests/DeleteDeploymentTests.cs
@@ -5,31 +5,95 @@ namespace Optionis.KPIs.Dashboard.Application.Tests
 {
     public class GIVEN_I_want_to_delete_a_deployment
     {
+        class TestRunner : DeploymentDeletionService.IDeleteDeployments
+        {
+            public bool DeploymentDeleted { get; private set; }
+            public bool ReleaseDeleted { get; private set; }
+
+            public TestRunner (int deploymentId)
+            {
+                new DeploymentDeletionService(() => DeploymentDeleted = true, this).Delete(deploymentId);
+            }
+
+            public void Delete(int deploymentId, Action onDeploymentDeleted)
+            {
+                DeploymentDeleted = true;
+            }
+        }
+
         public class WHEN_the_deployment_does_not_exist
         {
+            readonly TestRunner _testRunner = new TestRunner(-1);
+
             [Test]
-            public void THEN_success_is_reported()
+            public void THEN_deployment_deleted_delegate_is_executed()
             {
-                var releaseDeleted = false;
-                var svc = new DeploymentDeletionService (() => releaseDeleted = true);
-                svc.Delete (1);
-                Assert.IsTrue (releaseDeleted);
+                Assert.IsTrue (_testRunner.DeploymentDeleted);
+            }
+
+
+            [Test]
+            public void AND_the_release_is_not_deleted()
+            {
+                Assert.IsFalse (_testRunner.ReleaseDeleted);
+            }
+        }
+
+        public class WHEN_the_deployment_does_exist_AND_there_are_multiple_deployments_associated_with_the_release
+        {
+            TestRunner _testRunner = new TestRunner (1);
+
+            [Test]
+            public void THEN_the_deployment_is_deleted()
+            {
+                Assert.IsTrue (_testRunner.DeploymentDeleted);
+            }
+
+            [Test]
+            public void AND_the_release_is_not_deleted()
+            {
+                Assert.IsFalse (_testRunner.ReleaseDeleted);
+            }
+        }
+
+        public class WHEN_the_deployment_exists_AND_there_are_no_more_deployments_assigned_to_the_release
+        {
+            
+            readonly TestRunner _testRunner = new TestRunner (1);
+
+            [Test]
+            public void THEN_the_deployment_is_deleted()
+            {
+                Assert.IsTrue (_testRunner.DeploymentDeleted);
+            }
+
+            [Test]
+            public void AND_the_owning_release_is_not_deleted()
+            {
+                Assert.IsTrue (_testRunner.ReleaseDeleted);
             }
         }
 
         public class DeploymentDeletionService
         {
             readonly Action _onDeploymentDeleted;
+            readonly IDeleteDeployments _repository;
 
-            public DeploymentDeletionService (Action onDeploymentDeleted)
+            public interface IDeleteDeployments
             {
+                void Delete(int deploymentId, Action onDeploymentDeleted);
+            }
+
+            public DeploymentDeletionService (Action onDeploymentDeleted, IDeleteDeployments repository)
+            {
+                _repository = repository;
                 _onDeploymentDeleted = onDeploymentDeleted;
                 
             }
 
             public void Delete (int deploymentId)
             {
-                _onDeploymentDeleted ();
+                _repository.Delete (deploymentId, _onDeploymentDeleted);
             }
         }
     }
