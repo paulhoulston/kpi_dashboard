1f5c0aad04f73096f30da08600b446400941e8f1
Paul Houlston <paul.houlston@parasolgroup.co.uk>
2016-07-01 08:27:18 +0100

    Fixed create release problem

--- Optionis.KPIs.Dashboard/Optionis.KPIs.DataAccess/ReleaseCreator.cs
+++ Optionis.KPIs.Dashboard/Optionis.KPIs.DataAccess/ReleaseCreator.cs
@@ -5,9 +5,10 @@ namespace Optionis.KPIs.DataAccess
 {
     public class ReleaseCreator : ReleseCreationService.ICreateReleases
     {
-        public const string INSERT_RELEASE_SQL = @"INSERT INTO Releases(Application, Comments, Created, CreatedBy, Title) VALUES ('@application', '@comments', @created, '@createdBy', '@title')";
-        public const string INSERT_DEPLOYMENT_SQL = @"INSERT INTO Deployments(ReleaseId, DeploymentDate, DeploymentStatus, Version) VALUES (@releaseId, @deploymentDate, '@deploymentStatus', '@version')";
-        public const string INSERT_ISSUE_SQL = @"INSERT INTO Issues(ReleaseId, IssueId, Link, Title) VALUES (@releaseId, '@issueId', '@link', '@title')";
+        public const string INSERT_RELEASE_SQL = @"INSERT INTO Releases(Application, Comments, Created, CreatedBy, Title) VALUES (@application, @comments, @created, @createdBy, @title); SELECT SCOPE_IDENTITY()";
+        public const string INSERT_DEPLOYMENT_SQL = @"INSERT INTO Deployments(ReleaseId, DeploymentDate, DeploymentStatus, Version) VALUES (@releaseId, @deploymentDate, @deploymentStatus, @version)";
+        public const string INSERT_ISSUE_SQL = @"INSERT INTO Issues(ReleaseId, IssueId, Link, Title) VALUES (@releaseId, @issueId, @link, @title)";
+
         public void Create(ReleseCreationService.ReleaseToCreate model, Action<int> onReleaseCreated)
         {
             var dbWrapper = new DbWrapper();
@@ -28,13 +29,13 @@ namespace Optionis.KPIs.DataAccess
             });
 
             model.Issues.ForEachNullSafe(issue =>
-           dbWrapper.ExecuteScalar(INSERT_ISSUE_SQL, new
-           {
-               releaseId,
-               issueId = issue.Id,
-               link = issue.Link,
-               title = issue.Title
-           }));
+               dbWrapper.ExecuteScalar(INSERT_ISSUE_SQL, new
+               {
+                   releaseId,
+                   issueId = issue.Id,
+                   link = issue.Link,
+                   title = issue.Title
+               }));
 
             onReleaseCreated(releaseId);
         }
