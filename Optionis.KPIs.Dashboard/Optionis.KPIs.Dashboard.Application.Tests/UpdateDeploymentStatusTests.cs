ed0e7ee1169057aed8f7b17e717d6d2d220c557d
Paul Houlston <paul.houlston@parasolgroup.co.uk>
2016-07-08 06:54:03 +0100

    Added failing test for updating deployment status

---
+++ Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard.Application.Tests/UpdateDeploymentStatusTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Optionis.KPIs.Dashboard.Application.Tests
+{
+    public class GIVEN_I_want_to_update_a_deployment_status
+    {
+        public class WHEN_I_specify_a_status_for_a_valid_deployment
+        {
+            [Test]
+            public void THEN_the_status_is_updated()
+            {
+                var statusUpdated = false;
+                Assert.IsTrue(statusUpdated);
+            }
+        }
+    }
+}
