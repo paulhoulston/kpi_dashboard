82c47e36a257828218e2ebcbd97096712d1eafc7
Paul Houlston <paul.houlston@parasolgroup.co.uk>
2016-07-09 07:00:22 +0100

    All tests now pass

--- Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard.Application.Tests/UpdateDeploymentStatusTests.cs
+++ Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard.Application.Tests/UpdateDeploymentStatusTests.cs
@@ -3,23 +3,59 @@ using NUnit.Framework;
 
 namespace Optionis.KPIs.Dashboard.Application.Tests
 {
-    public class GIVEN_I_want_to_update_a_deployment_status
+    public class GIVEN_I_want_to_update_the_status_of_a_deployment
     {
+        class TestRunner : DeploymentStatusUpdaterService.ICheckIfDeploymentsExist, DeploymentStatusUpdaterService.IUpdateDeploymentStatuses
+        {
+            public bool _statusNotUpdated { get; private set; }
+            public bool _statusUpdated { get; private set; }
+            readonly bool _deploymentExists;
+
+            public TestRunner(bool deploymentExists)
+            {
+                _deploymentExists = deploymentExists;
+                new DeploymentStatusUpdaterService(
+                    () => _statusNotUpdated = true,
+                    () => _statusUpdated = true,
+                    this,
+                    this).UpdateStatus(1, DeploymentStatus.Aborted);
+            }
+
+            public void IfDeploymentExists(int deploymentId, Action onDeploymentNotExists, Action onDeploymentExists)
+            {
+                if (_deploymentExists)
+                    onDeploymentExists();
+                else
+                    onDeploymentNotExists();
+            }
+
+            public void Update(int deploymentId, DeploymentStatus status, Action onDeploymentUpdated)
+            {
+                onDeploymentUpdated();
+            }
+        }
+
         public class WHEN_I_specify_a_status_for_a_valid_deployment
         {
-            bool _statusUpdated;
-            readonly DeploymentStatusUpdaterService _service;
-
-            public WHEN_I_specify_a_status_for_a_valid_deployment()
-            {
-                _service = new DeploymentStatusUpdaterService(() => _statusUpdated = true);
-                _service.UpdateService();
-            }
+            readonly TestRunner _testRunner = new TestRunner(true);
 
             [Test]
             public void THEN_the_status_is_updated()
             {
-                Assert.IsTrue(_statusUpdated);
+                Assert.IsFalse(_testRunner._statusNotUpdated);
+                Assert.IsTrue(_testRunner._statusUpdated);
+            }
+        }
+
+        public class WHEN_the_deployment_cannot_be_found
+        {
+            readonly TestRunner _testRunner = new TestRunner(false);
+
+            [Test]
+            public void THEN_the_status_is_not_updated()
+            {
+                Assert.IsFalse(_testRunner._statusUpdated);
+                Assert.IsTrue(_testRunner._statusNotUpdated);
             }
         }
     }
@@ -27,15 +63,38 @@ namespace Optionis.KPIs.Dashboard.Application.Tests
     public class DeploymentStatusUpdaterService
     {
         readonly Action _onStatusUpdated;
+        readonly Action _statusNotUpdated;
+        readonly ICheckIfDeploymentsExist _repository;
+        readonly IUpdateDeploymentStatuses _updater;
 
-        public DeploymentStatusUpdaterService(Action onStatusUpdated)
+        public interface ICheckIfDeploymentsExist
         {
-            _onStatusUpdated= onStatusUpdated
+            void IfDeploymentExists(int deploymentId, Action onDeploymentExists, Action onDeploymentNotExists);
         }
 
-        public void UpdateService()
+        public interface IUpdateDeploymentStatuses
         {
-            _onStatusUpdated();
+            void Update(int deploymentId, DeploymentStatus status, Action onDeploymentUpdated);
+        }
+
+        public DeploymentStatusUpdaterService(
+            Action statusNotUpdated,
+            Action onStatusUpdated,
+            ICheckIfDeploymentsExist repository,
+            IUpdateDeploymentStatuses updater)
+        {
+            _onStatusUpdated = onStatusUpdated;
+            _statusNotUpdated = statusNotUpdated;
+            _repository = repository;
+            _updater = updater;
+        }
+
+        public void UpdateStatus(int deploymentId, DeploymentStatus status)
+        {
+            _repository.IfDeploymentExists(
+                deploymentId,
+                _statusNotUpdated,
+                () => _updater.Update(deploymentId, status, _onStatusUpdated));
         }
     }
 }
