170beaf39e26e49b2a582a3fca5556a11b1216c0
Paul Houlston <paul.houlston@parasolgroup.co.uk>
2016-07-16 16:50:31 +0100

    Added failing test for creating an issue

---
+++ Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard.Application.Tests/CreateIssueTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Optionis.KPIs.Dashboard.Application.Tests
+{
+    public class GIVEN_I_want_to_create_an_issue
+    {
+        public class WHEN_I_supply_all_valid_properties
+        {
+            [Test]
+            public void THEN_the_issue_is_created()
+            {
+                bool issueCreated = false;
+                Assert.IsTrue(issueCreated);
+            }
+        }
+    }
+}
