6b178b4d671e122f93b095d286f6bf901671b759
Paul Houlston <paul.houlston@parasolgroup.co.uk>
2016-07-19 06:11:57 +0100

    Added teste to check length of issue title doesn't exceed 255 characters

--- Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard.Application.Tests/CreateIssueTests.cs
+++ Optionis.KPIs.Dashboard/Optionis.KPIs.Dashboard.Application.Tests/CreateIssueTests.cs
@@ -94,14 +94,14 @@ namespace Optionis.KPIs.Dashboard.Application.Tests
         [TestFixture(true, "CR-123456789012345678901")]
         [TestFixture(true, "CR-1234567890123456789012")]
         [TestFixture(false, "CR-12345678901234567890123")]
-        public class WHEN_the_issue_exceeds_25_characters
+        public class WHEN_the_issue_id_exceeds_25_characters
         {
             bool _issueCreated;
             IssueCreationService.ValidationError? _validationError;
             readonly IssueCreationService _service;
             readonly bool _isValid;
 
-            public WHEN_the_issue_exceeds_25_characters(bool isValid, string issueId)
+            public WHEN_the_issue_id_exceeds_25_characters(bool isValid, string issueId)
             {
                 _isValid = isValid;
                 _service = new IssueCreationService(error => _validationError = error, () => _issueCreated = true);
@@ -125,6 +125,41 @@ namespace Optionis.KPIs.Dashboard.Application.Tests
                     Assert.AreEqual(IssueCreationService.ValidationError.InvalidIssueId, _validationError);
             }
         }
+
+        [TestFixture(true, "01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123")]
+        [TestFixture(true, "012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234")]
+        [TestFixture(false, "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345")]
+        public class WHEN_the_issue_title_exceeds_255_characters
+        {
+            bool _issueCreated;
+            IssueCreationService.ValidationError? _validationError;
+            readonly IssueCreationService _service;
+            readonly bool _isValid;
+
+            public WHEN_the_issue_title_exceeds_255_characters(bool isValid, string title)
+            {
+                _isValid = isValid;
+                _service = new IssueCreationService(error => _validationError = error, () => _issueCreated = true);
+                _service.Create(new IssueCreationService.Issue
+                {
+                    IssueId = "CR-56734",
+                    Title = title
+                });
+            }
+
+            [Test]
+            public void THEN_the_issue_is_not_created()
+            {
+                Assert.AreEqual(_isValid, _issueCreated);
+            }
+
+            [Test]
+            public void AND_a_issue_too_long_validation_message_is_returned()
+            {
+                if (!_isValid)
+                    Assert.AreEqual(IssueCreationService.ValidationError.InvalidTitle, _validationError);
+            }
+        }
     }
 
     public class IssueCreationService
@@ -139,7 +174,8 @@ namespace Optionis.KPIs.Dashboard.Application.Tests
             None = 0,
             EmptyIssueId = 1,
             EmptyTitle = 2,
-            InvalidIssueId = 3
+            InvalidIssueId = 3,
+            InvalidTitle = 4
         }
 
         public class Issue
@@ -158,7 +194,8 @@ namespace Optionis.KPIs.Dashboard.Application.Tests
                     CreateIssue,
                     new ValidateIssueIdIsNotEmpty(),
                     new ValidateIssueIdLength(),
-                    new ValidateTitleNotEmpty());
+                    new ValidateTitleNotEmpty(),
+                    new ValidateTitleLength());
         }
 
         public void Create(Issue issue)
@@ -201,6 +238,16 @@ namespace Optionis.KPIs.Dashboard.Application.Tests
             public bool IsValid(Issue issue)
             {
                 return !string.IsNullOrEmpty(issue.Title);
+            }
+        }
+
+        class ValidateTitleLength : IValidateObjects<Issue, ValidationError>
+        {
+            public ValidationError ValidationError { get { return ValidationError.InvalidTitle; } }
+
+            public bool IsValid(Issue issue)
+            {
+                return issue.Title.Length <= 255;
             }
         }
     }
